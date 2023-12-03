4d05580d16064c7fbb2896284cfddd43d62bd48d
Provat <142022951+provat37@users.noreply.github.com>
2023-12-03 22:14:46 +0600

    Assignment

---
+++ assignment.js
@@ -0,0 +1,76 @@
+১. Variable কি ?
+
+কম্পিউটার প্রোগ্রামিং এর ভাষায় ভ্যারিয়েবল হচ্ছে কোনো ডাটা বা ভ্যালু স্টোর করার জন্য কম্পিউটার মেমোরি লোকেশনের জন্য নির্দিষ্ট নাম। যাতে করে আমরা কোনো ভ্যালু কে কম্পিউটার মেমোরি তে স্টোর করে রাখতে পারি, প্রয়োজনে কল করতে পারি এবং চেঞ্জ বা মোডিফাই করতে পারি।
+
+২. Variable কিভাবে লিখতে হয় ?
+
+var friendsName = "provat";
+console.log(friendsName)
+
+৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+প্রোগ্রামিং-এ টেক্সট ভ্যালুকে টেক্সট স্ট্রিং বলা হয় |
+
+var strType = 'Provat Chakma';
+console.log(strType)
+
+৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+
+প্রোগ্রামিং-এ নাম্বার ভ্যালুকে নাম্বার variable বলা হয় | 
+
+var numType = 6789;
+console.log(numType)
+
+৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+
+বুলিয়ান সংখ্যা পদ্ধতি হল true বা false, এই 2টি মান বা value দ্বারা গঠিত সংখ্যা পদ্ধতি। বুলিয়ান নয় এমন কোন মান বা value কে বুলিয়ান মানে পরিবর্তন করতে জাভাস্ক্রিপ্ট এর বুলিয়ান অবজেক্ট ব্যবহার করা হয়।
+
+var boolType = true;
+console.log(boolType)
+
+৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+
+var rochana = "lis simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+console.log(rochana.toUpperCase())
+
+৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+
+অপারেটর হল বিভিন্ন গাণিতিক অপারেসন করার জন্য ব্যবহৃত বিভিন্ন চিহ্ন। যেমন - যোগ, বিয়োগ, গুন, ভাগ ইত্যাদি হল গাণিতিক অপারেসন।
+
+var x = 15;
+var y = 10;
+var z = x + y;
+console.log(z);
+
+
+৮. Math.abs() এর ব্যাবহার লিখুন । 
+
+var num1 = -50;
+var sum = Math.abs(num1)
+console.log(sum)
+
+৯. Math.celi()  এর ব্যাবহার লিখুন । 
+
+var  voter = 0.000001;
+var sum = Math.ceil(voter)
+console.log(sum)
+
+১০. Math.Floor() এর ব্যাবহার লিখুন । 
+
+var  voter = 0.000001;
+var sum = Math.floor(voter)
+console.log(sum)
+
+১১. Math.round() এর ব্যাবহার লিখুন ।
+
+var  voter = 0.599999;
+var sum = Math.round(voter)
+console.log(sum)
+
+১২. Math.random() এর ব্যাবহার লিখুন ।
+
+var  voter = 0.000001;
+var sum = Math.random(voter) * 100;
+console.log(sum)
+ 
+asignment.js - GitHub Link 
